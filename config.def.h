1c22ffb7baae4c81b3e5c40a71a8d5e1d538a0ce
SOS <heigvd.jeb@gmail.com>
2021-07-08 14:56:51 -0700

    tilegap patch

--- config.def.h
+++ config.def.h
@@ -2,10 +2,11 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int gappx     = 8;       /* gap pixel between windows */
 static const char *fonts[]          = { "Hasklig:size=11" };
 static const char dmenufont[]       = "Hasklig:size=11";
 static const char col_gray1[]       = "#222222";
