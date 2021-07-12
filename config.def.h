317e6daed205907555e66a9838e6faa37723ac9a
SOS <heigvd.jeb@gmail.com>
2021-07-12 08:15:53 -0700

    fuzzy

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -11,7 +12,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#282c34", "#979eab" },
+	[SchemeSelHighlight] = { "#e06c75", "#222222" },
+        [SchemeNormHighlight] = { "#e5c07b", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
